2ea52ea3280f64f7326014acb2201927e74880a9
lurno <lasse@lafjell.no>
2019-05-08 19:03:51 +0200

    Changing compass digital readout to three digits. IMHO thats more appealing and readable, and more correct.

--- plugins/dashboard_pi/src/dashboard_pi.cpp
+++ plugins/dashboard_pi/src/dashboard_pi.cpp
@@ -2412,11 +2412,11 @@ void DashboardWindow::SetInstrumentList( wxArrayInt list )
                 break;
             case ID_DBP_I_COG:
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_COG, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_COG, _T("%03.0f") );
                 break;
             case ID_DBP_M_COG:
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_MCOG, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_MCOG, _T("%03.0f") );
                 break;
             case ID_DBP_D_COG:
                 instrument = new DashboardInstrument_Compass( this, wxID_ANY,
@@ -2445,11 +2445,11 @@ void DashboardWindow::SetInstrumentList( wxArrayInt list )
             case ID_DBP_I_HDT: //true heading
                 // TODO: Option True or Magnetic
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_HDT, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_HDT, _T("%03.0f") );
                 break;
             case ID_DBP_I_HDM:  //magnetic heading
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_HDM, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_HDM, _T("%03.0f") );
                 break;
             case ID_DBP_D_AW:
             case ID_DBP_D_AWA:
